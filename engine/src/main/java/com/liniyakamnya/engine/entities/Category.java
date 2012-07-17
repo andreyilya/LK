a9cdd86a966fbf83ff3a266bed84b5abce35ceb7
andreyilya <liniyakamnya@gmail.com>
2012-07-17 15:27:59 +0300

    Entities

--- engine/src/main/java/com/liniyakamnya/engine/entities/Category.java
+++ engine/src/main/java/com/liniyakamnya/engine/entities/Category.java
@@ -1,5 +1,6 @@
 package com.liniyakamnya.engine.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @Entity
 public class Category implements Serializable {
     private int id;
+    private String name;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,5 +24,14 @@ public class Category implements Serializable {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    @Column
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
