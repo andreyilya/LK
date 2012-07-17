d786d76637f26a33a459db09f74c561cbd01424c
andreyilya <liniyakamnya@gmail.com>
2012-07-17 12:12:33 +0300

    System Entities

--- engine/src/main/java/com/liniyakamnya/engine/entities/User.java
+++ engine/src/main/java/com/liniyakamnya/engine/entities/User.java
@@ -1,5 +1,6 @@
 package com.liniyakamnya.engine.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,9 @@ import javax.persistence.Id;
 @Entity
 public class User {
     private int id;
+    private String login;
+    private String password;
+    private String email;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,5 +25,32 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    @Column
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Column
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
