88717a9d760665b19e31a0a969b377a49d66dbfd
Ilya Katlinsky <ilya.katlinsky@gmail.com>
2012-07-17 22:23:12 +0300

    Commit fix

--- engine/src/main/java/com/liniyakamnya/engine/dao/EntityDao.java
+++ engine/src/main/java/com/liniyakamnya/engine/dao/EntityDao.java
@@ -1,13 +1,24 @@
 package com.liniyakamnya.engine.dao;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * @author a.radkov
  *         Date: 16.07.12
  */
-public interface EntityDao {
+public interface EntityDAO<T extends Serializable> {
     void create(Object entity);
 
     void update(Object entity);
 
+    T safeOrUpdate(T entity);
+
     void delete(Object entity);
+
+    void delete(Long id);
+
+    T findById(Long id);
+
+    List<T> getAll();
 }
