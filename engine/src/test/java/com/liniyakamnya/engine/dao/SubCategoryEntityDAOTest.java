398a6ece551d67ff23d80558d5ee2a3530c021b0
andreyilya <liniyakamnya@gmail.com>
2012-07-19 13:06:48 +0300

    test directory fix

---
+++ engine/src/test/java/com/liniyakamnya/engine/dao/SubCategoryEntityDAOTest.java
@@ -0,0 +1,71 @@
+package com.liniyakamnya.engine.dao;
+
+import com.liniyakamnya.engine.entities.Category;
+import com.liniyakamnya.engine.entities.SubCategory;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * User: Asus
+ * Date: 19.07.12
+ * Time: 0:01
+ */
+public class SubCategoryEntityDAOTest {
+    private static SubCategoryEntityDAO entityDAO = new SubCategoryEntityDAO();
+    private static CategoryEntityDAO categoryEntityDAO = new CategoryEntityDAO();
+
+    @Test
+    public void testCreate() throws Exception {
+        entityDAO.create(new SubCategory());
+    }
+
+    @Test
+    public void testSafeOrUpdate() throws Exception {
+        Category category = new Category();
+        category.setName("testCategory");
+        Category createdCategory = categoryEntityDAO.safeOrUpdate(category);
+
+        SubCategory subCategory = new SubCategory();
+        subCategory.setName("testSubCategory");
+        subCategory.setCategory(createdCategory);
+        SubCategory createdSubCategory = entityDAO.safeOrUpdate(subCategory);
+
+        createdCategory.getSubCategories().add(createdSubCategory);
+        Category updatedCategory = categoryEntityDAO.safeOrUpdate(createdCategory);
+
+        assertNotNull(createdSubCategory);
+        assertNotNull(createdSubCategory.getCategory());
+
+        assertTrue(updatedCategory.getSubCategories().size() > 0);
+
+        categoryEntityDAO.delete(updatedCategory);
+        entityDAO.delete(createdSubCategory);
+
+    }
+
+    @Test
+    public void testFindById() throws Exception {
+        SubCategory subCategory = new SubCategory();
+        Long id = entityDAO.safeOrUpdate(subCategory).getId();
+        SubCategory created = entityDAO.findById(id);
+        assertNotNull(created);
+
+        deleteSubCategory(created);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        List<SubCategory> categories = entityDAO.getAll();
+
+        assertNotNull(categories);
+    }
+
+    private void deleteSubCategory(SubCategory subCategory) {
+        entityDAO.delete(subCategory);
+    }
+}
