8f383fb5d317c9a7bed76e915e910221cd0e84bb
andreyilya <liniyakamnya@gmail.com>
2012-07-19 10:17:39 +0300

    Test for relation one - to - many for Category - Subcategory

--- engine/src/test/com/liniyakamnya/engine/dao/SubCategoryEntityDAOTest.java
+++ engine/src/test/com/liniyakamnya/engine/dao/SubCategoryEntityDAOTest.java
@@ -1,8 +1,11 @@
 package com.liniyakamnya.engine.dao;
 
+import com.liniyakamnya.engine.entities.Category;
 import com.liniyakamnya.engine.entities.SubCategory;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
@@ -14,10 +17,36 @@ import static org.junit.Assert.assertNotNull;
  */
 public class SubCategoryEntityDAOTest {
     private static SubCategoryEntityDAO entityDAO = new SubCategoryEntityDAO();
+    private static CategoryEntityDAO categoryEntityDAO = new CategoryEntityDAO();
 
     @Test
     public void testCreate() throws Exception {
         entityDAO.create(new SubCategory());
+    }
+
+    @Test
+    public void testSafeOrUpdate() throws Exception {
+        Category category = new Category();
+        category.setName("testCategory");
+        Category createdCategory = categoryEntityDAO.safeOrUpdate(category);
+
+        SubCategory subCategory = new SubCategory();
+        subCategory.setName("testSubCategory");
+        subCategory.setCategory(createdCategory);
+        SubCategory createdSubCategory = entityDAO.safeOrUpdate(subCategory);
+
+        createdCategory.setSubCategories(new ArrayList<SubCategory>());
+        createdCategory.getSubCategories().add(createdSubCategory);
+        Category updatedCategory = categoryEntityDAO.safeOrUpdate(createdCategory);
+
+        assertNotNull(createdSubCategory);
+        assertNotNull(createdSubCategory.getCategory());
+
+        assertTrue(updatedCategory.getSubCategories().size() > 0);
+
+        categoryEntityDAO.delete(updatedCategory);
+        entityDAO.delete(createdSubCategory);
+
     }
 
     @Test
