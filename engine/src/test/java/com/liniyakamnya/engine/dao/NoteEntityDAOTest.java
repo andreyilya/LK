3dcd2e8d2b4af0856f7c845e2c76bd02d18ae713
andreyilya <liniyakamnya@gmail.com>
2012-07-19 16:55:42 +0300

    Note entity dao test

---
+++ engine/src/test/java/com/liniyakamnya/engine/dao/NoteEntityDAOTest.java
@@ -0,0 +1,39 @@
+package com.liniyakamnya.engine.dao;
+
+import org.junit.Test;
+
+/**
+ * @author a.radkov
+ *         Date: 19.07.12
+ */
+public class NoteEntityDAOTest {
+    @Test
+    public void testCreate() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testSafeOrUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testFindById() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDomainClass() throws Exception {
+
+    }
+}
