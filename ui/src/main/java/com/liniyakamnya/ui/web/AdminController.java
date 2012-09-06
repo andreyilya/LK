1560b5a8ae980f7532726d4764751ec7d1d6a1ae
andreyilya <liniyakamnya@gmail.com>
2012-09-06 18:50:56 +0300

    Example of role creating

--- ui/src/main/java/com/liniyakamnya/ui/web/AdminController.java
+++ ui/src/main/java/com/liniyakamnya/ui/web/AdminController.java
@@ -6,8 +6,10 @@ import com.liniyakamnya.ui.entities.Role;
 import com.liniyakamnya.ui.entities.User;
 import com.liniyakamnya.ui.utils.Paramerers;
 import com.liniyakamnya.ui.utils.URLs;
+
 import java.util.Map;
 import javax.inject.Named;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -26,41 +28,45 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class AdminController {
 
-	private static final String USER = "user";
-	private static final String USER_ID = "userId";
+    private static final String USER = "user";
+    private static final String USER_ID = "userId";
 
-	@Autowired
-	@Named(Paramerers.USER_DAO)
-	private EntityDAO<User> userEntityDAO;
+    @Autowired
+    @Named(Paramerers.USER_DAO)
+    private EntityDAO<User> userEntityDAO;
 
-	@Autowired
-	@Named(Paramerers.ROLE_DAO)
-	private EntityDAO<Role> roleEntityDAO;
+    @Autowired
+    @Named(Paramerers.ROLE_DAO)
+    private EntityDAO<Role> roleEntityDAO;
 
-	@RequestMapping(URLs.ADMIN)
-	public String listUsers(Map<String, Object> map) {
-		map.put(Paramerers.USER, new User());
-		map.put(Paramerers.ACTIONS, Actions.values());
-		map.put(Paramerers.ROLES, roleEntityDAO.getAll());
-		map.put(Paramerers.USER_LIST, userEntityDAO.getAll());
-		return URLs.ADMIN_PAGE;
-	}
+    @RequestMapping(URLs.ADMIN)
+    public String listUsers(Map<String, Object> map) {
+        map.put(Paramerers.USER, new User());
+        map.put(Paramerers.ACTIONS, Actions.values());
+        map.put(Paramerers.ROLES, roleEntityDAO.getAll());
+        map.put(Paramerers.USER_LIST, userEntityDAO.getAll());
+        return URLs.ADMIN_PAGE;
+    }
 
-	@RequestMapping(value = URLs.ADD, method = RequestMethod.POST)
-	public
-	@ResponseBody
-	String addUser(@ModelAttribute(USER) User user,
-				 BindingResult result) {
-        user.getRoles().add(roleEntityDAO.findById(Long.parseLong(user.getRoless().split(",")[0])));
-		return userEntityDAO.safeOrUpdate(user).toString();
-	}
+    @RequestMapping(value = URLs.ADD, method = RequestMethod.POST)
+    public
+    @ResponseBody
+    String addUser(@ModelAttribute(USER) User user,
+                   BindingResult result) {
+        //Correctly, it should be moved to service , byt for out small project it`s very lazy
+        //to create service lajer. probably, it will be done.
+        for (String id : user.getRoless().split(",")) {
+            user.getRoles().add(roleEntityDAO.findById(Long.parseLong(id)));
+        }
+        return userEntityDAO.safeOrUpdate(user).toString();
+    }
 
-	@RequestMapping(URLs.DELETE_USER)
-	public
-	@ResponseBody
-	String deleteUser(@PathVariable(USER_ID) Long userId) {
+    @RequestMapping(URLs.DELETE_USER)
+    public
+    @ResponseBody
+    String deleteUser(@PathVariable(USER_ID) Long userId) {
 
-		userEntityDAO.delete(userId);
-		return URLs.ADMIN_REDIRECT;
-	}
+        userEntityDAO.delete(userId);
+        return URLs.ADMIN_REDIRECT;
+    }
 }
