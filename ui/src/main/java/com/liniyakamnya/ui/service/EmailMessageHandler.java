9cb23cd8374b8af9150ee5789a7954e17f0757e4
andreyilya <liniyakamnya@gmail.com>
2012-09-13 09:18:38 +0300

    Asynchronus email sending

--- ui/src/main/java/com/liniyakamnya/ui/service/EmailMessageHandler.java
+++ ui/src/main/java/com/liniyakamnya/ui/service/EmailMessageHandler.java
@@ -1,18 +1,14 @@
 package com.liniyakamnya.ui.service;
 
 import com.liniyakamnya.ui.dao.EntityDAO;
-import com.liniyakamnya.ui.email.EmailMessageGenerator;
-import com.liniyakamnya.ui.email.EmailSender;
 import com.liniyakamnya.ui.entities.Actions;
 import com.liniyakamnya.ui.entities.User;
 import com.liniyakamnya.ui.utils.Parameters;
-import org.apache.commons.mail.EmailException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Named;
-import java.util.List;
 
 /**
  * @author a.radkov
@@ -27,18 +23,8 @@ public class EmailMessageHandler implements EmailService {
 
     @Override
     public void protocolAction(Actions action) {
-        try {
-            List<User> users = userEntityDAO.getAll();
-            for (User user : users) {
-                if (user.getActionsForEmail().isProtocol(action)) {
-                    String message = EmailMessageGenerator.generate(action);
-                    EmailSender.sendMail(user.getEmail(), message);
-                }
-            }
-        } catch (EmailException e) {
-            e.printStackTrace();
-        } catch (NullPointerException e){
-			e.printStackTrace();
-		}
+        EmailThread emailThread = new EmailThread(userEntityDAO,action);
+        Thread thread = new Thread(emailThread);
+        thread.start();
     }
 }
---
+++ ui/src/main/java/com/liniyakamnya/ui/service/EmailThread.java
@@ -0,0 +1,42 @@
+package com.liniyakamnya.ui.service;
+
+import com.liniyakamnya.ui.dao.EntityDAO;
+import com.liniyakamnya.ui.dao.UserEntityDAO;
+import com.liniyakamnya.ui.email.EmailMessageGenerator;
+import com.liniyakamnya.ui.email.EmailSender;
+import com.liniyakamnya.ui.entities.Actions;
+import com.liniyakamnya.ui.entities.User;
+import org.apache.commons.mail.EmailException;
+
+import java.util.List;
+
+public class EmailThread implements Runnable {
+    private final EntityDAO<User> entityDAO;
+    private final Actions action;
+
+    public EmailThread(EntityDAO<User> entityDAO, Actions action) {
+        this.entityDAO = entityDAO;
+        this.action = action;
+    }
+
+    @Override
+    public void run() {
+        protocolAction();
+    }
+
+    private void protocolAction() {
+        try {
+            List<User> users = entityDAO.getAll();
+            for (User user : users) {
+                if (user.getActionsForEmail().isProtocol(action)) {
+                    String message = EmailMessageGenerator.generate(action);
+                    EmailSender.sendMail(user.getEmail(), message);
+                }
+            }
+        } catch (EmailException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+}
